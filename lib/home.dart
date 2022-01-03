6b2f14a7e30683a693361413aab2c2e63368016c
Bonface2 <bonnym2031@gmail.com>
2022-01-03 14:20:11 +0300

    UI improvements

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:tflite/tflite.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:image_picker/image_picker.dart';
+import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -80,142 +81,143 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('Dr.Skin',
+      appBar: NewGradientAppBar(
+        title: Text('Dr. Skin',
             style: TextStyle(
-              color: Colors.blue,
-              fontSize: 26,
-              fontWeight: FontWeight.bold,
-            )),
-        centerTitle: true,
+                color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 25)),
+        gradient: LinearGradient(
+          colors: [
+            Colors.white,
+            Colors.blueGrey,
+          ],
+        ),
       ),
       body: Container(
-        color: Colors.white,
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 50),
-        child: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(30),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                child: Center(
-                  child: _loading == true
-                      ? null //show nothing if no picture selected
-                      : Container(
-                          child: Column(
-                            children: [
-                              Container(
-                                height: 400,
-                                width: 300,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(30),
-                                  child: Image.file(
-                                    _image,
-                                    fit: BoxFit.fill,
-                                  ),
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(30),
+        decoration: new BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.white,
+            Colors.brown,
+          ],
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Center(
+                child: _loading == true
+                    ? null //show nothing if no picture selected
+                    : Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 400,
+                              width: 300,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: Image.file(
+                                  _image,
+                                  fit: BoxFit.fill,
                                 ),
                               ),
-                              Divider(
-                                height: 25,
-                                thickness: 1,
-                              ),
-                              // ignore: unnecessary_null_comparison
-                              _output != null
-                                  ? Text(
-                                      'Diagnosis: ${_output[0]['label']}.',
-                                      style: TextStyle(
-                                          color: Colors.brown[400],
-                                          fontSize: 23,
-                                          fontWeight: FontWeight.w600),
-                                      textAlign: TextAlign.center,
-                                    )
-                                  : Container(),
-                              Divider(
-                                height: 25,
-                                thickness: 1,
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => Info()),
-                                  );
-                                },
-                                child: Container(
-                                  width:
-                                      MediaQuery.of(context).size.width - 200,
-                                  alignment: Alignment.center,
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 24, vertical: 17),
-                                  decoration: BoxDecoration(
-                                      color: Colors.blueGrey[600],
-                                      borderRadius: BorderRadius.circular(15)),
-                                  child: Text(
-                                    'Learn More',
+                            ),
+                            Divider(
+                              height: 25,
+                              thickness: 1,
+                            ),
+                            // ignore: unnecessary_null_comparison
+                            _output != null
+                                ? Text(
+                                    'Diagnosis: ${_output[0]['label']}.',
                                     style: TextStyle(
-                                        color: Colors.white, fontSize: 16),
-                                  ),
+                                        color: Colors.white,
+                                        fontSize: 23,
+                                        fontWeight: FontWeight.w600),
+                                    textAlign: TextAlign.center,
+                                  )
+                                : Container(),
+                            Divider(
+                              height: 25,
+                              thickness: 1,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Info()),
+                                );
+                              },
+                              child: Container(
+                                width: MediaQuery.of(context).size.width - 200,
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 24, vertical: 17),
+                                decoration: BoxDecoration(
+                                    color: Colors.blueGrey[600],
+                                    borderRadius: BorderRadius.circular(15)),
+                                child: Text(
+                                  'Learn More',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16),
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                ),
-              ),
-              Container(
-                child: _loading == false
-                    ? null //show nothing if a picture is selected
-                    : Column(
-                        children: [
-                          GestureDetector(
-                            onTap: pickImage,
-                            child: Container(
-                              width: MediaQuery.of(context).size.width - 200,
-                              alignment: Alignment.center,
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 24, vertical: 17),
-                              decoration: BoxDecoration(
-                                  color: Colors.blueGrey[600],
-                                  borderRadius: BorderRadius.circular(15)),
-                              child: Text(
-                                'Take A Photo',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 16),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          GestureDetector(
-                            onTap: pickGalleryImage,
-                            child: Container(
-                              width: MediaQuery.of(context).size.width - 200,
-                              alignment: Alignment.center,
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 24, vertical: 17),
-                              decoration: BoxDecoration(
-                                  color: Colors.blueGrey[600],
-                                  borderRadius: BorderRadius.circular(15)),
-                              child: Text(
-                                'Pick From Gallery',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 16),
-                              ),
-                            ),
-                          ),
-                        ],
                       ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              child: _loading == false
+                  ? null //show nothing if a picture is selected
+                  : Column(
+                      children: [
+                        GestureDetector(
+                          onTap: pickImage,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width - 200,
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 17),
+                            decoration: BoxDecoration(
+                                color: Colors.blueGrey[600],
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Text(
+                              'Take A Photo',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        GestureDetector(
+                          onTap: pickGalleryImage,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width - 200,
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 17),
+                            decoration: BoxDecoration(
+                                color: Colors.blueGrey[600],
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Text(
+                              'Pick From Gallery',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+            ),
+          ],
         ),
       ),
     );
@@ -249,19 +251,25 @@ class _Info extends State<Info> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+        appBar: NewGradientAppBar(
           title: Text('Diagnosis Information',
               style: TextStyle(
-                color: Colors.blue,
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-              )),
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25)),
+          gradient: LinearGradient(
+            colors: [
+              Colors.blueGrey,
+              Colors.white,
+            ],
+          ),
           centerTitle: true,
         ),
         body: ListView(
           padding: const EdgeInsets.all(8),
           children: <Widget>[
             Container(
+              margin: const EdgeInsets.all(10),
               child: Center(
                   child: Text(
                 information,
