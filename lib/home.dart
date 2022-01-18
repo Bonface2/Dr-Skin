7cdc06ad3361184a1ef2732963e5186039f80890
Bonface2 <bonnym2031@gmail.com>
2022-01-18 14:15:51 +0300

    Minor ui changes

--- lib/home.dart
+++ lib/home.dart
@@ -93,6 +93,7 @@ class _HomeState extends State<Home> {
           ],
         ),
         // automaticallyImplyLeading: false,
+        iconTheme: IconThemeData(color: Colors.black),
       ),
       body: Container(
         alignment: Alignment.center,
